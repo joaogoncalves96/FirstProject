fec040fe16494ff767c602e880aedcd469ec12a8
joaogoncalves96 <joao.goncalves@mindswap.academy>
2022-02-21 16:18:47 +0000

    Client-Server

--- src/academy/mindswap/Client/Player.java
+++ src/academy/mindswap/Client/Player.java
@@ -1,9 +1,68 @@
 package academy.mindswap.Client;
 
-public class Player {
-    public static void main(String[] args) {
-        System.out.println("hahaha");
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
 
-        String hello;
+public class Player implements Runnable {
+
+    //public static ArrayList<Player> playerArrayList = new ArrayList<>();
+    private Socket socket;
+    private String hostName = "localHost";
+    private int portNumber = 8080;
+    private String clientUsername;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public Player() {
+        try {
+            this.socket = new Socket(hostName, portNumber);
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.clientUsername = bufferedReader.readLine();
+           // playerArrayList.add(this);
+            // broadcastMessage("SERVER: " + clientUsername + " has entered the table!");
+        } catch (IOException e) {
+            closeAll(socket, bufferedReader, bufferedWriter);
+        }
     }
-}
+
+        @Override
+        public void run () {
+            String messageFromClient;
+
+            while (socket.isConnected()) {
+                try {
+                    messageFromClient = bufferedReader.readLine();
+                    broadcastMessage(messageFromClient);
+                } catch (IOException e) {
+                    closeAll(socket, bufferedReader, bufferedWriter);
+                    break;
+                }
+            }
+        }
+
+        // ler mensagens, mandar mensagens,
+       /* public void broadcastMessage(String messageToSend) {
+            for (Player player : playerArrayList) {
+                try {
+                    if(!player.clientUsername.equals(clientUsername)) {
+                        player.bufferedWriter.write(messageToSend);
+                        player.bufferedWriter.newLine();
+                        player.bufferedWriter.flush();
+                    }
+                } catch (IOException e) {
+                   closeAll(socket, bufferedReader, bufferedWriter);
+                }
+            }*/
+        }
+        public void removePlayer() {
+        playerArrayList.remove( this);
+        broadcastMessage("SERVER: " + clientUsername + " has left the table");
+        }
+
+        public void closeAll(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        removePlayer();
+
+        }
+    }
--- src/academy/mindswap/Main.java
+++ src/academy/mindswap/Main.java
@@ -4,8 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("HELLO WORLD");
-        System.out.println("Hello world2");
+
 
     }
 }
